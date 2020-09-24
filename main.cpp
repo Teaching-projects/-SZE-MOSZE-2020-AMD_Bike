d7582fcf8bad89b484b2e79f5fda13ed8f34f386
Skrapits Róbert <skrapits.robert99@gmail.com>
2020-09-24 21:02:19 +0200

    Change cout and added to yml file

--- main.cpp
+++ main.cpp
@@ -4,26 +4,19 @@
 
 void Fight(Character character1, Character character2) {
 	int n = 0;
-	std::cout << character1.getName() << ' ' << character1.getHp() << ' ' << character1.getDmg() << std::endl;
-	std::cout << character2.getName() << ' ' << character2.getHp() << ' ' << character2.getDmg() << std::endl;
 	while (character1.getHp() != 0 && character2.getHp() != 0) {
-		std::cout << character1.getName() << ": HP: " << character1.getHp() << ", DMG: " << character1.getDmg() << std::endl;
-		std::cout << character2.getName() << ": HP: " << character2.getHp() << ", DMG: " << character2.getDmg() << std::endl;
 		if (n % 2 == 0) {
-			std::cout << character1.getName() << "->" << character2.getName() << std::endl;
 			character2.DMGTaken(character1);
 		}
 		else {
-			std::cout << character2.getName() << "->" << character1.getName() << std::endl;
 			character1.DMGTaken(character2);
 		}
 		n++;
 	}
-	std::cout << character1.getName() << ": HP: " << character1.getHp() << ", DMG: " << character1.getDmg() << std::endl;
-	std::cout << character2.getName() << ": HP: " << character2.getHp() << ", DMG: " << character2.getDmg() << std::endl;
+	
 
-	if (character1.IsDead()) { std::cout << character1.getName() << " died. " << character2.getName() << " wins." << std::endl; }
-	else { std::cout << character2.getName() << " died. " << character1.getName() << " wins." << std::endl; }
+	if (character1.IsDead()) { std::cout << character2.getName() << " wins. Remaining HP: " << character2.getHp() << std::endl; }
+	else { std::cout <<  character1.getName() << " wins. Remaining HP: " << character1.getHp() << std::endl; }
 }
 
 bool IsExist(const std::string FileName1, const std::string FileName2) {
