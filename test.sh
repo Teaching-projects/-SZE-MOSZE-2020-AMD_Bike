4252ee1177ee0301dd4f1857ce92e75e4b582791
hajdunorbi <norbert998@citromail.hu>
2020-09-24 22:30:28 +0200

    Create units folder, add new character and change sh file

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-./a.out Vader.json Luke.json >> output.txt
+IFS=$'\n'
+
+./a.out units/Vader.json units/Luke.json >> output.txt
+./a.out units/Vader.json units/Yoda.json >> output.txt
+./a.out units/Yoda.json units/Luke.json >> output.txt
+./a.out units/Yoda.json units/Vader.json >> output.txt
+./a.out units/Luke.json units/Yoda.json >> output.txt
+./a.out units/Luke.json units/Vader.json >> output.txt
+
+cat output.txt
