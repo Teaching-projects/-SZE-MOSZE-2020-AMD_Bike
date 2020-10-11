7a86fd05b5c1424871e2f259793746b5a4dcbece
hajdunorbi <norbert998@citromail.hu>
2020-10-11 14:37:39 +0200

    Create attackspeed branch

--- Character.h
+++ Character.h
@@ -9,14 +9,17 @@ private:
 	const std::string name;
 	int hp;
 	const int dmg;
+	const double attackspeed;
+	void DMGTaken(const Character character);
+	friend void Fight(Character character1, Character character2);
 public:
-	Character(const std::string name, const int hp, const int dmg);
+	Character(const std::string name, const int hp, const int dmg, const double attackspeed);
 	std::string getName() const;
 	int getHp() const;
 	int getDmg() const;
-	void DMGTaken(const Character character);
+	double getAttackspeed() const;
 	bool IsDead() const;
-	static Character parseUnit(const std::string& FileName);
+	static Character parseUnit(const std::string FileName);
 };
 
 #endif
