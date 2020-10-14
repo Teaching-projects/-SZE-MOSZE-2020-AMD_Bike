c100506221b338ac16e3b5f57e37a1496b5db7b1
Szabi11 <szabolcs.gergaly11@gmail.com>
2020-10-14 16:10:03 +0200

    Create the levelup branch

--- main.cpp
+++ main.cpp
@@ -1,56 +1,47 @@
-#include<iostream>
 #include "Character.h"
-#include<fstream>
+#include "Player.h"
+#include <iostream>
 
-void Fight(Character character1, Character character2) {
+void Fight(Character& fighter1, Character& fighter2) {
 	double gameprogress = 0;
-	double aktAS1 = character1.getAttackspeed();
-	double aktAS2 = character2.getAttackspeed();
+	double aktAS1 = fighter1.getAttackspeed();
+	double aktAS2 = fighter2.getAttackspeed();
 
-	character2.DMGTaken(character1);
-	if (!character2.IsDead()) {
-		character1.DMGTaken(character2);
+	fighter1.OnePunch(fighter2);
+	if (!fighter2.IsDead()) {
+		fighter2.OnePunch(fighter1);
 	}
-	while (character1.getHp() != 0 && character2.getHp() != 0) {
-		if ((aktAS1-gameprogress) < (aktAS2-gameprogress)) {
-			character2.DMGTaken(character1);
+	while (!fighter1.IsDead() && !fighter2.IsDead()) {
+		if ((aktAS1 - gameprogress) < (aktAS2 - gameprogress)) {
+			fighter1.OnePunch(fighter2);
 			gameprogress = aktAS1;
-			aktAS1 += character1.getAttackspeed();
+			aktAS1 += fighter1.getAttackspeed();
 		}
-		else if ((aktAS1) - gameprogress > (aktAS2 - gameprogress)) {
-			character1.DMGTaken(character2);
+		else if ((aktAS1)-gameprogress > (aktAS2 - gameprogress)) {
+			fighter2.OnePunch(fighter1);
 			gameprogress = aktAS2;
-			aktAS2 += character2.getAttackspeed();
+			aktAS2 += fighter2.getAttackspeed();
 		}
 		else {
-			character2.DMGTaken(character1);
-			if (!character2.IsDead()) {
-				character1.DMGTaken(character2);
+			fighter1.OnePunch(fighter2);
+			if (!fighter2.IsDead()) {
+				fighter2.OnePunch(fighter1);
 			}
 			gameprogress = aktAS1;
-			aktAS1 += character1.getAttackspeed();
-			aktAS2 += character2.getAttackspeed();
+			aktAS1 += fighter1.getAttackspeed();
+			aktAS2 += fighter2.getAttackspeed();
 		}
 	}
-	if (character1.IsDead()) { std::cout << character2.getName() << " wins. Remaining HP: " << character2.getHp() << std::endl; }
-	else { std::cout << character1.getName() << " wins. Remaining HP: " << character1.getHp() << std::endl; }
-}
-
-bool IsExist(const std::string FileName1, const std::string FileName2) {
-	std::ifstream file1, file2;
-	file1.open(FileName1);
-	file2.open(FileName2);
-	if (!file1.is_open() || !file2.is_open()) {
-		file1.close();
-		file2.close();
-		return true;
+	if (fighter2.IsDead()) {
+		std::cout << fighter1.getName() << " wins. Remaining HP: " << fighter1.getHp() << std::endl;
 	}
-	else { file1.close(); file2.close(); return false; }
+	else {
+		std::cout << fighter2.getName() << " wins. Remaining HP: " << fighter2.getHp() << std::endl;
+	}
 }
-
 
 int main(int argc, char *argv[]) {
-	
+
 	if (argc < 3) {
 		std::cout << "Not enough arguments" << std::endl;
 		return 1;
@@ -59,16 +50,18 @@ int main(int argc, char *argv[]) {
 		std::cout << "Too many arguments" << std::endl;
 		return 1;
 	}
-	else if (IsExist(argv[1], argv[2])) {
-		std::cout << "At least one file not exists" << std::endl;
-		return 1;
-	}
 	else {
-		Character Character1 = Character::parseUnit(argv[1]);
-		Character Character2 = Character::parseUnit(argv[2]);
-		Fight(Character1, Character2);
-
+		try
+		{
+			Player Fighter1 = Player::parsePlayer(argv[1]);
+			Character Fighter2 = Character::parseUnit(argv[2]);
+			Fight(Fighter1, Fighter2);
+		}
+		catch (const int& e)
+		{
+			std::cout << "One of the given arguments does not exists." << std::endl;
+			return e;
+		}
 	}
-
 	return 0;
 }
