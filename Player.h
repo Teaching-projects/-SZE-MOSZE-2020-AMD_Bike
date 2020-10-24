ff764a84181df22aa07daaea58bc862997cd6c4b
Szabi11 <szabolcs.gergaly11@gmail.com>
2020-10-24 16:12:42 +0200

    Now OnePunch is virtual and Fight is correct

--- Player.h
+++ Player.h
@@ -13,7 +13,7 @@ protected:
 public:
 	Player(const std::string &name, const int hp, int dmg, const int xp = 0) : Character(name, hp, dmg), xp(xp), MaxHp(hp) {}
 	static Player parsePlayer(const std::string& FileName);
-	void OnePunch(Character& enemy);
+	void OnePunch(Character& enemy) override;
 };
 
 #endif
