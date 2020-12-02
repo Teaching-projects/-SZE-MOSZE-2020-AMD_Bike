8f11c80ad8a3227c8f2074480ea573da175ad058
Szabi11 <szabolcs.gergaly11@gmail.com>
2020-12-02 15:33:49 +0100

    Try to fix test.sh

--- test.sh
+++ test.sh
@@ -12,17 +12,3 @@ then
 else
 	echo "Matching"
 fi
-
-IFS=$'\n'
-rm output.txt
-cat test/scenarios/scenario1game.txt | ./main >> output.txt
-cat output.txt
-DIFF=$(diff output.txt scenario1game_output.txt) 
-
-if [ "$DIFF" != "" ] 
-then
-	echo "Not matching"
-	exit 1
-else
-	echo "Matching"
-fi
